9d7d6a526dec49eb8d45ef038302f1a93d6a8012
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-12-15 11:52:57 +0700

    chore: setup github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wiormiw.github.io',
+  base: '/portfolio',
   integrations: [react()],
 
   vite: {
